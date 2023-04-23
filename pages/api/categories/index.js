f9ed09e11ca43bf3a4bdfc24d5d577a489c76c7d
David Perelman <dperelman3@gmail.com>
2023-04-24 02:24:43 +0300

    set categories api endpoint

---
+++ pages/api/categories/index.js
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Add Mongo URI to .env.local');
+}
+
+export default async function handler(req, res) {
+  try {
+    const mongoClient = await new MongoClient(uri, options).connect();
+    console.log('Connected!');
+
+    const db = mongoClient.db('myFirstDatabase');
+
+    const collection = db.collection('categories');
+
+    const results = await collection
+      .find({})
+      .project({ _id: 0, title: 0 })
+      .toArray();
+
+    res.status(200).json(results);
+  } catch (error) {}
+}
